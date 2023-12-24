1ba592927e97ee45520e732031c2db69745ad53e
bonyoji <vdt.vincent.ta@gmail.com>
2023-12-24 21:18:29 +0900

    Add button press to input display function
    Press button to input number to display

--- script.js
+++ script.js
@@ -17,5 +17,46 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    
+    switch(operator) {
+        case '+' :
+            add(a, b);
+            break;
+        case '-' :
+            subtract(a, b);
+            break;
+        case 'x' :
+            multiply(a, b);
+            break;
+        case '/' :
+            divide(a, b);
+    }
 }
+
+const numPad = document.querySelectorAll('.num');
+const operatorPad = document.querySelectorAll('.operator');
+
+numPad.forEach(btnNum => {
+    btnNum.addEventListener('click', strToDisplay);
+})
+
+operatorPad.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', strToDisplay);
+})
+
+const topDisplay = document.querySelector('.top-display');
+const operatorDisplay = document.querySelector('.mid-display');
+const bottomDisplay = document.querySelector('.bottom-display');
+
+const displayNum = {};
+let displayVal = '0';
+
+function strToDisplay(e) {
+    let btnText = e.target.innerText;
+    if (displayVal == '0') {
+        displayVal = '';
+    }
+    displayVal += btnText;
+    bottomDisplay.innerText = displayVal;
+}
+
+
